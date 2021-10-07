bc07d4bc1c9dfdbb2b73c866eefaf8d2a1f8c375
Zuzia <s25093@pjwstk.edu.pl>
2021-10-07 09:38:43 +0200

    commit

---
+++ s01-hello-me.cpp
@@ -0,0 +1,13 @@
+/*
+ * The very first program you should write in any new programming language.
+ */
+
+
+#include <iostream>
+
+
+auto main() -> int
+{
+    std::cout << "Hello, Zuzia!\n";
+    return 0;
+}
