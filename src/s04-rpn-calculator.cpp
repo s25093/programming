7b041f3b32d524532bf854b620a4001ec8dd7d17
zuzannaciecielag <91730793+zuzannaciecielag@users.noreply.github.com>
2021-10-15 12:57:01 +0200

    fixed whole number division, mod, power, sqrt and log

--- src/s04-rpn-calculator.cpp
+++ src/s04-rpn-calculator.cpp
@@ -9,6 +9,7 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <cmath>
 #include <math.h>
 
 
@@ -22,17 +23,7 @@ auto pop_top(std::stack<double>& stack) -> double
     return element;
 }
 
-auto pop_top_int(std::stack<int>& stack) -> int
-{
-    if (stack.empty()) {
-        throw std::logic_error{"empty stack"};
-    }
-    auto element = std::move(stack.top());
-    stack.pop();
-    return element;
-}
-
-
+//works
 auto evaluate_addition(std::stack<double>& stack) -> void
 {
     if (stack.size() < 2) {
@@ -43,6 +34,7 @@ auto evaluate_addition(std::stack<double>& stack) -> void
     stack.push(a + b);
 }
 
+//works
 auto evaluate_subtraction(std::stack<double>& stack) -> void
 {
     if (stack.size() < 2) {
@@ -53,64 +45,84 @@ auto evaluate_subtraction(std::stack<double>& stack) -> void
     stack.push(a - b);
 }
 
+//works
 auto evaluate_multiplication(std::stack<double>& stack) -> void
 {
-if (stack.size() < 2) {
-throw std::logic_error{"not enough operands for -"};
-}
-auto const b = pop_top(stack);
-auto const a = pop_top(stack);
-stack.push(a * b);
+    if (stack.size() < 2) {
+        throw std::logic_error{"not enough operands for *"};
+    }
+    auto const b = pop_top(stack);
+    auto const a = pop_top(stack);
+    stack.push(a * b);
 }
 
+//works
 auto evaluate_division(std::stack<double>& stack) -> void
 {
-if (stack.size() < 2) {
-throw std::logic_error{"not enough operands for -"};
-}
-auto const b = pop_top(stack);
-auto const a = pop_top(stack);
-stack.push(a / b);
+    if (stack.size() < 2) {
+        throw std::logic_error{"not enough operands for /"};
+    }
+    auto const b = pop_top(stack);
+    auto const a = pop_top(stack);
+    stack.push(a / b);
 }
 
-auto evaluate_whole_number_division(std::stack<int>& stack) -> void
+//works
+auto evaluate_whole_number_division(std::stack<double>& stack) -> void
 {
-if (stack.size() < 2) {
-throw std::logic_error{"not enough operands for -"};
-}
-auto const b = pop_top_int(stack);
-auto const a = pop_top_int(stack);
-stack.push(a / b);
+    if (stack.size() < 2) {
+        throw std::logic_error{"not enough operands for //"};
+    }
+    auto const b = pop_top(stack);
+    auto const a = pop_top(stack);
+    stack.push(a / b);
 }
 
-auto evaluate_modulo(std::stack<int>& stack) -> void
+
+//works
+auto evaluate_modulo(std::stack<double>& stack) -> void
 {
-if (stack.size() < 2) {
-throw std::logic_error{"not enough operands for -"};
-}
-auto const b = pop_top_int(stack);
-auto const a = pop_top_int(stack);
-stack.push(a % b);
+    if (stack.size() < 2) {
+        throw std::logic_error{"not enough operands for %"};
+    }
+    auto const b = pop_top(stack);
+    auto const a = pop_top(stack);
+    auto const c = fmod(a, b);
+    stack.push(c);
 }
 
+//works
 auto evaluate_power(std::stack<double>& stack) -> void
 {
-if (stack.size() < 2) {
-throw std::logic_error{"not enough operands for -"};
-}
-auto const b = pop_top(stack);
-auto const a = pop_top(stack);
-stack.push(pow(a,b));
+    if (stack.size() < 2) {
+        throw std::logic_error{"not enough operands for pow"};
+    }
+    auto const b = pop_top(stack);
+    auto const a = pop_top(stack);
+    stack.push(pow(a, b));
 }
 
+//works (first the root, then the value)
 auto evaluate_sqrt(std::stack<double>& stack) -> void
 {
-if (stack.size() < 2) {
-throw std::logic_error{"not enough operands for -"};
+    if (stack.size() < 2) {
+        throw std::logic_error{"not enough operands for sqrt"};
+    }
+    auto const a = pop_top(stack);
+    auto const b = pop_top(stack);
+    stack.push(pow(a, 1/b));
 }
-auto const a = pop_top(stack);
-stack.push(sqrt(a));
+
+//works (natural logarithm)
+auto evaluate_log(std::stack<double>& stack) -> void
+{
+    if (stack.size() < 1) {
+        throw std::logic_error{"not enough operands for log"};
+    }
+    auto const a = pop_top(stack);
+    stack.push(log (a));
 }
+
 
 
 auto make_args(int argc, char* argv[]) -> std::vector<std::string>
@@ -134,20 +146,22 @@ auto main(int argc, char* argv[]) -> int
             } else if (each == "-") {
                 evaluate_subtraction(stack);
             } else if (each == "*") {
-            evaluate_multiplication(stack);
+                evaluate_multiplication(stack);
             } else if (each == "/") {
-            evaluate_division(stack);
+                evaluate_division(stack);
             } else if (each == "//") {
-            evaluate_whole_number_division(stack);
+                evaluate_whole_number_division(stack);
             } else if (each == "%") {
-            evaluate_modulo(stack);
+                evaluate_modulo(stack);
             } else if (each == "**") {
-            evaluate_power(stack);
+                evaluate_power(stack);
             } else if (each == "sqrt") {
-            evaluate_sqrt(stack);
+                evaluate_sqrt(stack);
+            } else if (each == "log") {
+                evaluate_log(stack);
             } else {
                 stack.push(std::stod(each));
-            }
+            } 
         } catch (std::logic_error const& e) {
             std::cerr << "error: " << each << ": " << e.what() << "\n";
         }
