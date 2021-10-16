6434bce632740f027cab6debbbfdd046fb8ecb65
zuzannaciecielag <91730793+zuzannaciecielag@users.noreply.github.com>
2021-10-16 15:59:52 +0200

    added times of day to time

--- src/s03-Time.cpp
+++ src/s03-Time.cpp
@@ -2,64 +2,121 @@
 #include <string>
 #include "Time.h"
 
-Time currentTime1(14, 21, 50);
+Time currentTime1(23, 59, 59);
+bool runCheck = true;
+
+std::string currentTimeOfDay;
+
+auto Time::enum_to_string(Time::timeOfDay a) const -> std::string {
+
+switch (a) {
+    case timeOfDay::Morning:
+        return "Morning";
+    case timeOfDay::Noon:
+        return "Noon";
+    case timeOfDay::Afternoon:
+        return "Afternoon";
+    case timeOfDay::Evening:
+        return "Evening";
+    case timeOfDay::Night:
+        return "Night";
+}
+
+}
+auto Time::time_of_day() const -> std::string {
+
+    if ((hour < 12) && (hour >= 6)){
+        currentTimeOfDay = enum_to_string(timeOfDay::Morning);
+    } else if (hour == 12) {
+        currentTimeOfDay = enum_to_string(timeOfDay::Noon);
+    } else if ((hour > 12) && (hour < 17)) {
+        currentTimeOfDay = enum_to_string(timeOfDay::Afternoon);
+    } else if ((hour >= 17) && (hour < 20)) {
+        currentTimeOfDay = enum_to_string(timeOfDay::Evening);
+    } else {
+        currentTimeOfDay = enum_to_string(timeOfDay::Night);
+    }
+
+    return currentTimeOfDay;
+}
 
 auto Time::next_hour() -> void {
-hour++;
-std::cout << hour << std::endl;
+    hour++;
+    if (hour >= 24) {
+        hour = 0;
+    }
 }
 
 auto Time::next_minute() -> void {
-minute++;
-std::cout << minute << std::endl;
+    minute++;
+    if (minute > 59) {
+        minute = 0;
+        hour++;
+        if (hour >= 24) {
+            hour = 0;
+        }
+    }
 }
 
 auto Time::next_second() -> void {
-second++;
-std::cout << second << std::endl;
+    second++;
+    if (second > 59) {
+        second = 0;
+        minute++;
+        if (minute > 59) {
+            minute = 0;
+            hour++;
+            if (hour >= 24) {
+                hour = 0;
+            }
+        }
+    }
 }
 
 auto Time::to_string() const -> std::string {
 
-std::cout << hour << ":" << minute << ":" << second << std::endl;
+    std::cout << hour << ":" << minute << ":" << second << std::endl;
 
-std::string input;
-std::cout << "Extend time by 1 hour, minute, or second? Type hour, minute, or second: " << std::endl;
-std::getline(std::cin, input);
+    std::cout << "It is now ";
+    std:: cout << time_of_day();
+    std::cout << '\n' << std::endl;
 
-if (input == "hour") {
+    std::string input;
+    std::cout << "Extend time by 1 hour, minute, or second? Type hour, minute, or second (or term to terminate): " << std::endl;
+    std::getline(std::cin, input);
 
-std::cout << "Next hour: ";
-currentTime1.next_hour();
-std::cout << ":" << minute << ":" << second << std::endl;
+    if (input == "hour") {
 
-} else if (input == "minute") {
+        std::cout << "Next hour: ";
+        currentTime1.next_hour();
 
-std::cout << "Next minute: ";
-std::cout << hour << ":"; 
-currentTime1.next_minute();
-std::cout << ":" << second << std::endl;
+    } else if (input == "minute") {
 
-} else if (input == "second") {
+        std::cout << "Next minute: ";
+        currentTime1.next_minute();
 
-std::cout << "Next second: ";
-std::cout << hour << ":" << minute << ":";
-currentTime1.next_second();
+    } else if (input == "second") {
 
-} else {
+        std::cout << "Next second: ";
+        currentTime1.next_second();
 
-std::cout << "Invalid input." << std::endl;
+    } else if (input == "term") {
 
-}
+        runCheck = false;
 
-return "End.";
+    } else {
+        std::cout << "Invalid input." << std::endl;
+
+    }
+
+    return " ";
 
 }
 
 auto main () -> int {
 
-std::cout << currentTime1.to_string() << std::endl;
-
-return 0;
+    while (runCheck) {
+        std::cout << currentTime1.to_string() << std::endl;
+    }
 
 }
