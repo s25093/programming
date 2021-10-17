d30a7ebcdafc6dedd21ac57ae6ffa17d96ef1466
zuzannaciecielag <91730793+zuzannaciecielag@users.noreply.github.com>
2021-10-17 17:07:00 +0200

    Update TimeTemp.h

--- include/TimeTemp.h
+++ include/TimeTemp.h
@@ -6,16 +6,16 @@ struct Time {
 
     enum class timeOfDay{Morning, Noon, Afternoon, Evening, Night};
 
-    auto to_string() const -> std::string;
+    auto to_string() -> std::string;
     auto time_of_day() const -> std::string;
     auto enum_to_string(timeOfDay) const -> std::string;
 
-    auto operator+ ( Time const &) const -> Time ;
-    auto operator- ( Time const &) const -> Time ;
-    auto operator< ( Time const &) const -> bool ;
-    auto operator> ( Time const &) const -> bool ;
-    auto operator== ( Time const &) const -> bool ;
-    auto operator!= ( Time const &) const -> bool ;
+    auto operator+ ( Time &)  -> Time ;
+    auto operator- ( Time &)  -> Time ;
+    auto operator< ( Time const &)  -> bool ;
+    auto operator> ( Time const &)  -> bool ;
+    auto operator== ( Time const &)  -> bool ;
+    auto operator!= ( Time const &)  -> bool ;
 
     auto next_hour() -> void;
     auto next_minute() -> void;
